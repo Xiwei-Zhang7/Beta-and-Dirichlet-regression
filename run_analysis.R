21df3dbbcf121278ed68eacd4c821c2a93c1d06e
Xiwei Zhang <sundazhi203@yahoo.com>
2026-09-03 11:51:34 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 # Full analysis workflow
 
 
-source("R/01_data_preprocessing.R")
+source("R/1_data_preprocessing.R")
 
-source("R/02_beta_regression.R")
+source("R/2_beta_regression.R")
 
-source("R/03_dirichlet_regression.R")
+source("R/3_dirichlet_regression.R")
 
-source("R/04_model_selection.R")
+source("R/4_model_selection.R")
 
-source("R/05_g7_models.R")
+source("R/5_g7_models.R")
 
-source("R/06_model_evaluation.R")
+source("R/6_model_evaluation.R")
