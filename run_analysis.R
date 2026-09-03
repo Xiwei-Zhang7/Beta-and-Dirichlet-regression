3a41a83c84ab27b4bcd8fb0502f9000c634894ef
Xiwei Zhang <sundazhi203@yahoo.com>
2026-09-03 10:51:13 -0400

    End-to-end analysis runner

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+# Full analysis workflow
+
+
+source("R/01_data_preprocessing.R")
+
+source("R/02_beta_regression.R")
+
+source("R/03_dirichlet_regression.R")
+
+source("R/04_model_selection.R")
+
+source("R/05_g7_models.R")
+
+source("R/06_model_evaluation.R")
